dfa2f19c241b27094c52bbacc12b892efcd5e3c8
Eric Hainer <eric.hainer@gmail.com>
2017-06-22 21:35:44 -0600

    [CHG] Code style changes

--- lib/generators/voltron/crop/install/assets_generator.rb
+++ lib/generators/voltron/crop/install/assets_generator.rb
@@ -4,18 +4,18 @@ module Voltron
       module Install
         class AssetsGenerator < Rails::Generators::Base
 
-          source_root File.expand_path("../../../../../../", __FILE__)
+          source_root File.expand_path('../../../../../../', __FILE__)
 
-          desc "Install Voltron Crop assets"
+          desc 'Install Voltron Crop assets'
 
           def copy_javascripts_assets
-            copy_file "app/assets/javascripts/voltron-crop.js", Rails.root.join("app", "assets", "javascripts", "voltron-crop.js")
-            copy_file "app/assets/javascripts/cropit.js", Rails.root.join("app", "assets", "javascripts", "cropit.js")
-            copy_file "app/assets/javascripts/simple-slider.js", Rails.root.join("app", "assets", "javascripts", "simple-slider.js")
+            copy_file 'app/assets/javascripts/voltron-crop.js', Rails.root.join('app', 'assets', 'javascripts', 'voltron-crop.js')
+            copy_file 'app/assets/javascripts/cropit.js', Rails.root.join('app', 'assets', 'javascripts', 'cropit.js')
+            copy_file 'app/assets/javascripts/simple-slider.js', Rails.root.join('app', 'assets', 'javascripts', 'simple-slider.js')
           end
 
           def copy_stylesheets_assets
-            copy_file "app/assets/stylesheets/voltron-crop.scss", Rails.root.join("app", "assets", "stylesheets", "voltron-crop.scss")
+            copy_file 'app/assets/stylesheets/voltron-crop.scss', Rails.root.join('app', 'assets', 'stylesheets', 'voltron-crop.scss')
           end
 
         end
